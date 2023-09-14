11c4196b7ab8c7ab775fc3f2dfc279da94517a55
Jacques Zwiegelaar <jacquesz@threepeaks.co.za>
2023-09-14 08:08:36 +0200

    Fix Query

--- ManagementDashboard/SQLQueries/Risk/DeposotNotCoveredEFTDO.sql
+++ ManagementDashboard/SQLQueries/Risk/DeposotNotCoveredEFTDO.sql
@@ -3,6 +3,13 @@
 com_ref as 'Account Reference',
 (select sum(tbl_accounting_depost_tracking.amount)   from tbl_accounting_depost_tracking where tbl_accounting_depost_tracking.cref = com_ref and tbl_accounting_depost_tracking.deposit_date <= '{endDate}') as 'Deposit',
 (select sum(rbr_h_amnt_ok) from tblrbr where rbr_comref  = com_ref and rbr_date between '{startDate}' and '{endDate}') as 'Collection',
+
+
+
+ifnull(round((((select sum(tbl_accounting_depost_tracking.amount)   from tbl_accounting_depost_tracking where tbl_accounting_depost_tracking.cref = com_ref and tbl_accounting_depost_tracking.deposit_date <= '{endDate}') /
+(select sum(rbr_h_amnt_ok) from tblrbr where rbr_comref  = com_ref and rbr_date between '{startDate}' and '{endDate}')) * 100),2),0) as 'Deposit Ratio',
+
+
 (select sum(dbt_amount) from tbldebits where dbt_comref = com_ref and dbt_pass_unpaid in (2,3) and dbt_unpaid_datetime  between '{startDate}' and '{endDate}') as 'Unpaids',
 
 round(
