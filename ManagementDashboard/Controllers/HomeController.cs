31cdcc7d57ed48f4cb9e280e8701e7edc82b9771
Jacques Zwiegelaar <jacquesz@bytetribe.co.za>
2025-12-28 09:03:08 +0200

    Refactor action date logic into dedicated classes
    Encapsulate action date processing in SubmissionsActionDate and SubmissionsActionDates classes. Move counting, validation, and list-building logic out of HomeController for improved organization and maintainability. Update HomeController to use the new classes, simplifying the Index action and enhancing separation of concerns.

--- ManagementDashboard/Controllers/HomeController.cs
+++ ManagementDashboard/Controllers/HomeController.cs
@@ -11,74 +11,250 @@ using System.Web.Mvc;
 
 namespace ManagementDashboard.Controllers
 {
-
-    [Authorize]
-    [LogUserAccess]
-    public class HomeController : Controller
+    public class SubmissionsActionDate
     {
-        public ActionResult Index()
+        public DateTime ActionDate { get; set; }
+        public int Count { get; set; }
+        public bool IsValidDate { get; set; }
+        public void SetIfIsValidActionDate()
         {
-            var vm = new HomeViewModel();
+            this.IsValidDate = Helpers.DateHelpers.IsValidActionDate(this.ActionDate);
+        }
+            
 
 
+        private void CountRunsNotSend()
+        {
+            string actionDateSQL = this.ActionDate.ToString("yyyy-MM-dd");
+            string query = $"select count(*) as c from tblrbr left join tblhyphen_batchno on hbn_rbr = rbr_id and hbn_type = 1 where ";
+            query += $"rbr_date = '{actionDateSQL}' ";
+            query += $"and rbr_status in (0, 1) and hbn_rbr is null";
 
-            var actionDates = new List<DateTime>();
-            var actionDatesMax = 5;
-            var actionDateCounter = -1;
-            var lastActionDate = Helpers.DateHelpers.GetPreviousActionDate(DateTime.Now, 1).Date;
-            actionDates.Add(lastActionDate);
-            while (actionDateCounter < actionDatesMax)
+
+            var db = new DBConnect();
+
+            var dbResult = db.Query(query);
+            DataRow dr = dbResult.Tables[0].Rows[0];
+
+            //Type t = a["c"].GetType();
+            Int64 count = dr.Field<Int64>("c");
+            this.Count = Convert.ToInt32(count);
+        }
+        public void Build()
+        {
+            CountRunsNotSend();
+            this.IsValidDate = Helpers.DateHelpers.IsValidActionDate(this.ActionDate);
+        }
+    }
+
+    public class SubmissionsActionDates : List<SubmissionsActionDate>
+    {
+        private bool isBuild;
+        public void AddMissingNonProcessingDates()
+        {
+            var minDate = this.Min(x => x.ActionDate);
+            var maxDate = this.Max(x => x.ActionDate);
+
+            var currentDate = minDate;
+
+            while (currentDate < maxDate)
             {
-                actionDateCounter++;
-                var nextActionDate = Helpers.DateHelpers.GetNextActionDate(DateTime.Now, actionDateCounter).Date;
-                actionDates.Add(nextActionDate);
+                if (!this.Any(x => x.ActionDate.Date == currentDate.Date))
+                {
+                    this.Add(new SubmissionsActionDate
+                    {
+                        ActionDate = currentDate,
+                        IsValidDate = false
+                    });
+                }
+
+                currentDate = currentDate.AddDays(1);
             }
+        }
+
+        public void ReOrderList()
+        {
+            var tempList = this.OrderBy(x => x.ActionDate).ToList();
+            this.Clear();
+            this.AddRange(tempList);
+        }
+        public void AddSubmissionDates(List<DateTime> allSubmissionActionDates)
+        {
+            foreach (var item in allSubmissionActionDates)
+            {
+                if (!this.Any(x => x.ActionDate == item))
+                {
+                    this.Add(new SubmissionsActionDate()
+                    {
+                        ActionDate = item,
+                        IsValidDate = false
+                    });
+                }
+
+
+
+            }
+        }
+
+        public List<ActionDateCountWidget> ToWidgets()
+        {
+            if (!isBuild)
+                throw new Exception("Build not executed before ToWidgets");
 
             var actionDateWidgets = new List<ActionDateCountWidget>();
-            foreach (var actionDate in actionDates)
+
+            var widgets = new List<ActionDateCountWidget>();
+            var minValidActionDate = this.Where(x => x.IsValidDate).Min(x => x.ActionDate);
+
+            foreach (var sad in this)
             {
-                bool includePast = false;
-                if (actionDate < DateTime.Now.Date)
-                   includePast = true;
-
-
-                var kvp = CountRunsNotSend(actionDate, includePast);
-
 
                 string level = "bg-success";
-                string title = "Action Date " + actionDate.ToString("dd/MM/yyyy");
-                if (actionDate < DateTime.Now.Date)
+                string title = "Action Date " + sad.ActionDate.ToString("dd/MM/yyyy");
+                if (sad.ActionDate < DateTime.Now.Date)
                 {
                     title += " (Past Due)";
                     level = "bg-danger";
-                    if (kvp.Value == 0)
+                    if (sad.Count == 0)
                         level = "bg-success";
                 }
 
-                if (actionDate == DateTime.Now.Date)
+                var cutoffTimeForToday = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 15, 0, 0);
+                if (sad.ActionDate == DateTime.Now.Date && DateTime.Now < cutoffTimeForToday)
                 {
                     title += " (Today)";
                     level = "bg-warning";
-                    if (kvp.Value == 0)
+                    if (sad.Count == 0)
                         level = "bg-success";
-
+                }
+                if (sad.ActionDate == DateTime.Now.Date && DateTime.Now >= cutoffTimeForToday)
+                {
+                    title += " (Today)";
+                    level = "bg-danger";
+                    if (sad.Count == 0)
+                        level = "bg-success";
                 }
 
 
 
 
                 var actionDateWidget = new ActionDateCountWidget();
-                actionDateWidget.ActionDate = kvp.Key;
+                actionDateWidget.ActionDate = sad.ActionDate;
                 actionDateWidget.Title = title;
 
                 actionDateWidget.Level = level;
-                actionDateWidget.Count = kvp.Value;
+                actionDateWidget.Count = sad.Count;
 
-                
+
 
                 actionDateWidgets.Add(actionDateWidget);
+
             }
-            vm.ActionDateWidgets = actionDateWidgets.OrderBy(x=>x.ActionDate).ToList();
+
+
+            return actionDateWidgets
+                .Where(x=>x.Count > 0)
+                .OrderBy(x=>x.ActionDate).ToList(); 
+        }
+
+
+        public void Build(int maxActionDates = 4)
+        {
+            var validActionDates = new List<DateTime>();
+            var actionDatesMax = maxActionDates;
+            var actionDateCounter = 0;
+
+            var lastActionDate = Helpers.DateHelpers.GetPreviousActionDate(DateTime.Now, 1).Date;
+            this.Add(new SubmissionsActionDate()
+            {
+                ActionDate = lastActionDate,
+                IsValidDate = true
+            });
+
+            //TODO
+            if (Helpers.DateHelpers.IsValidActionDate(DateTime.Now))
+            {
+                this.Add(new SubmissionsActionDate()
+                {
+                    ActionDate = DateTime.Now.Date,
+                    IsValidDate = true
+                });
+            }
+
+            while (actionDateCounter < actionDatesMax)
+            {
+                actionDateCounter++;
+                var nextActionDate = Helpers.DateHelpers.GetNextActionDate(DateTime.Now, actionDateCounter).Date;
+                validActionDates.Add(nextActionDate);
+
+                this.Add(new SubmissionsActionDate()
+                {
+                    ActionDate = nextActionDate,
+                    IsValidDate = true
+                });
+            }
+
+            var maxActionDate = validActionDates.Max();
+            var allSubmissionActionDates = GetSubmissionActionDates(maxActionDate);
+            this.AddSubmissionDates(allSubmissionActionDates);
+
+            AddMissingNonProcessingDates();
+            ReOrderList();
+
+            foreach (var sad in this)
+            {
+                sad.Build();
+            }
+
+            //Set build flag = true
+            isBuild = true;
+
+        }
+
+        List<DateTime> GetSubmissionActionDates(DateTime maxActionDate)
+        {
+            var sqlDateString = maxActionDate.ToString("yyyy-MM-dd");   
+            string query = $"select distinct rbr_date as c from tblrbr left join tblhyphen_batchno on hbn_rbr = rbr_id and hbn_type = 1 where ";
+            query += $" rbr_status in (0, 1) and hbn_rbr is null and rbr_date <= '{sqlDateString}'";
+
+
+            var db = new DBConnect();
+
+            var dbResult = db.Query(query);
+            var result = new List<DateTime>();
+            foreach (DataRow dr in dbResult.Tables[0].Rows)
+            {
+                //Convert the c from MySQL DateTime to DateTime
+                DateTime actionDate = dr.Field<DateTime>("c");
+                result.Add(actionDate.Date);
+
+            }
+
+            return result;
+
+
+        }
+
+    }
+
+
+    [Authorize]
+    [LogUserAccess]
+    public class HomeController : Controller
+    {
+
+        List<ActionDateCountWidget> GetActionDateWidgets()
+        {
+            var submissionActionDates = new SubmissionsActionDates();
+            submissionActionDates.Build();
+            return submissionActionDates.ToWidgets();
+        }
+        public ActionResult Index()
+        {
+            var vm = new HomeViewModel();
+
+            vm.ActionDateWidgets = GetActionDateWidgets();
+
 
 
             
