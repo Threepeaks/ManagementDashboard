48c1b5a4e8966ada1c638df0dd9b329b92b04b72
Jacques Zwiegelaar <jacquesz@bytetribe.co.za>
2026-05-04 15:16:20 +0200

    Refactor SQL query for improved calculations and sorting
    Updated the SQL query to include a subquery for calculating the sum of `rbr_h_amnt_ok` to ensure accurate "UnpRatio" calculations. Renamed the column alias from `'Unp Ratio'` to `'UnpRatio'` for consistency. Added a new column `'Is Covered'` to evaluate deposit coverage. Modified the `WHERE` clause to include `com_acc_cancel in (0,1)` alongside `com_retterms = 3`. Changed the `ORDER BY` clause to sort results by `'UnpRatio'` in descending order for better prioritization.

--- ManagementDashboard/SQLQueries/Risk/DeposotNotCoveredEFTDO.sql
+++ ManagementDashboard/SQLQueries/Risk/DeposotNotCoveredEFTDO.sql
@@ -1,4 +1,4 @@
-﻿select 
+﻿select * from (select
 
 com_ref as 'Account Reference',
 (select sum(tbl_accounting_depost_tracking.amount)   from tbl_accounting_depost_tracking where tbl_accounting_depost_tracking.cref = com_ref and tbl_accounting_depost_tracking.deposit_date <= '{endDate}') as 'Deposit',
@@ -15,7 +15,7 @@ ifnull(round((((select sum(tbl_accounting_depost_tracking.amount)   from tbl_acc
 round(
 ifnull(
 ((select sum(dbt_amount) from tbldebits where dbt_comref = com_ref and dbt_pass_unpaid in (2,3) and dbt_unpaid_datetime  between '{startDate}' and '{endDate}') / 
-(select sum(rbr_h_amnt_ok) from tblrbr where rbr_comref  = com_ref and rbr_date between '{startDate}' and '{endDate}') ) * 100 ,0),2) as 'Unp Ratio',
+(select sum(rbr_h_amnt_ok) from tblrbr where rbr_comref  = com_ref and rbr_date between '{startDate}' and '{endDate}') ) * 100 ,0),2) as 'UnpRatio',
 
 if ((select sum(tbl_accounting_depost_tracking.amount)   from tbl_accounting_depost_tracking where tbl_accounting_depost_tracking.cref = com_ref and tbl_accounting_depost_tracking.deposit_date <= '{endDate}')  > 
 ifnull((select sum(dbt_amount) from tbldebits where dbt_comref = com_ref and dbt_pass_unpaid in (2,3) and dbt_unpaid_datetime  between '{startDate}' and '{endDate}'),0), "Yes","No") as 'Is Covered'
@@ -24,6 +24,6 @@ ifnull((select sum(dbt_amount) from tbldebits where dbt_comref = com_ref and dbt
 from tblcompany 
 
 where com_retterms = 3
-and com_acc_cancel in (0,1)
+and com_acc_cancel in (0,1) ) t
 
-order by com_ref
+order by t.UnpRatio desc
